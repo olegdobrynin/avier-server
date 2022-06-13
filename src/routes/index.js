8e45df23b208a7afd04ef599a3e430e2e36a684c
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-13 22:52:09 +0300

    feat: add type route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import Router from 'express';
+import typeRouter from './type.js';
 import userRouter from './user.js';
 
 const router = new Router();
 
+router.use('/type', typeRouter);
 router.use('/user', userRouter);
 
 export default router;
