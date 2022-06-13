5ec61210ca1483e5cd2577938fc8012dfd8df4b1
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-13 22:41:56 +0300

    migration: types

---
+++ src/migrations/20220607122102-create-types.cjs
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('types', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(20),
+        unique: true,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('types');
+  }
+};
