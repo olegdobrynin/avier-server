eaa80ac934ca10ab345cf9c8575548aff34ff1b9
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-13 23:26:52 +0300

    feat: add artist route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import Router from 'express';
+import artistRouter from './artist.js';
 import typeRouter from './type.js';
 import userRouter from './user.js';
 
 const router = new Router();
 
+router.use('/artist', artistRouter);
 router.use('/type', typeRouter);
 router.use('/user', userRouter);
 
