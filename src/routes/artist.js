f5d921de64f83412bf40dbe957e77f38eeb31210
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-14 00:59:57 +0300

    fix: swap routes

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -3,12 +3,12 @@ import ArtistController from '../controllers/artist.js';
 
 const router = new Router();
 
-router.route('/')
-  .get(ArtistController.getAll)
-  .post(ArtistController.create);
 router.route('/:id(\\d+)')
   .get(ArtistController.getOne)
   .patch(ArtistController.update)
   .delete(ArtistController.delete);
+router.route('/')
+  .get(ArtistController.getAll)
+  .post(ArtistController.create);
 
 export default router;
