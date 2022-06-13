0fcf831fab6e8d1b993cc142378b34026107b246
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-13 23:01:54 +0300

    feat: move from cjs to esm

---
+++ src/controllers/artist.js
@@ -0,0 +1,39 @@
+import { v4 } from 'uuid';
+import path from 'path';
+import models from '../models/index.js';
+import sequelize from '../db/db.js';
+import ApiError from '../errors/ApiError.js';
+
+const { Artist } = models;
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const buildImgPath = (imgName) => path.resolve(__dirname, '..', '..', 'static', 'artists', imgName);
+
+export default class ArtistController {
+  static async create(req, res, next) {
+    try {
+      await sequelize.transaction(async (transaction) => {
+        const { userId } = req.body;
+        const imgName = req.files?.img ? `${v4()}.jpg` : 'default.jpg';
+
+        const createParams = { ...req.body, user_id: userId, img: imgName };
+        const { id } = await Artist.create(createParams, { returning: ['id'], transaction });
+        await req.files?.img?.mv(buildImgPath(imgName));
+
+        res.status(201).json({ id });
+      });
+    } catch (error) {
+      next(new ApiError(error, 500));
+    }
+  }
+
+  static async getAll(_req, res, next) {
+    try {
+      const artists = await Artist.findAll({ attributes: ['id', 'name', 'img'] });
+
+      res.json(artists);
+    } catch (error) {
+      next(new ApiError(error, 500));
+    }
+  }
+}
