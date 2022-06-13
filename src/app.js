4d011b5daf48a9a934ee0d848bdc5f134dfb9e06
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-13 22:04:02 +0300

    feat: move app and replace cjs to esm

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import fileUpload from 'express-fileupload';
+import router from './routes/index.js';
+import errorHandler from './middlewares/errorHandling.js';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, '..', 'static')));
+app.use(fileUpload());
+app.use('/api', router);
+app.use(errorHandler);
+
+export default app;
