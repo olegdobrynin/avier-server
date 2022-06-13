dad8d8c17fb5a6c03b1a07db918c2eb5f57835d5
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-14 00:45:33 +0300

    feat: add mark route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import Router from 'express';
 import artRouter from './art.js';
 import artistRouter from './artist.js';
+import markRouter from './mark.js';
 import typeRouter from './type.js';
 import userRouter from './user.js';
 
@@ -8,6 +9,7 @@ const router = new Router();
 
 router.use('/art', artRouter);
 router.use('/artist', artistRouter);
+router.use('/mark', markRouter);
 router.use('/type', typeRouter);
 router.use('/user', userRouter);
 
