8c81f61687c26b5df6f8bf377c28e42ae260c1df
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-13 22:04:26 +0300

    build: connect notFound middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import path from 'path';
 import fileUpload from 'express-fileupload';
 import router from './routes/index.js';
+import notFoundHandler from './middlewares/notFound.js';
 import errorHandler from './middlewares/errorHandling.js';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '..', 'static')));
 app.use(fileUpload());
 app.use('/api', router);
+app.use(notFoundHandler);
 app.use(errorHandler);
 
 export default app;
