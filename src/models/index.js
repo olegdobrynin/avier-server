9bc47f5ba955f9e8b7947ed8cf6452d36237a5ee
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-15 16:55:50 +0300

    fix: windows compatibility

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,16 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { DataTypes } from 'sequelize';
-import sequelize from '../db/db.js'; // eslint-disable-line
+import sequelize from '../db/db.js';
 
-const { pathname } = new URL(import.meta.url);
-const { base: __filename, dir: __dirname } = path.parse(pathname);
+const __filename  = fileURLToPath(import.meta.url);
+const { base: basename, dir: __dirname } = path.parse(__filename);
 
 const promises = fs
   .readdirSync(__dirname)
-  .filter((file) => file.indexOf('.') !== 0 && file !== __filename && file.slice(-3) === '.js')
-  .map((file) => import(path.join(__dirname, file)));
+  .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+  .map((file) => import(`./${file}`)));
 
 const modules = await Promise.all(promises);
 const models = modules
