5163b4287ddbaa6b30131c3fa5df80e10040c37f
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-17 22:07:20 +0300

    migration: art_extra_imgs

---
+++ src/migrations/20220617182113-create-art_extra_imgs.cjs
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('art_extra_imgs', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      art_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'arts',
+        },
+      },
+      img: {
+        type: Sequelize.STRING(40),
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('art_extra_imgs');
+  },
+};
