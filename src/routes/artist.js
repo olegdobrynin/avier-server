7fc36714f31d9e57bb73eeeb294044a766eb13e2
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-24 01:48:12 +0300

    feat: move from fileUpload to multer

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -1,14 +1,17 @@
 import Router from 'express';
+import multer from 'multer';
+import { defaultLimits, fileFilter } from '../helpers/multer.js';
 import ArtistController from '../controllers/artist.js';
 
 const router = new Router();
+const upload = multer({ fileFilter, limits: { ...defaultLimits, fields: 3, files: 1 } });
 
 router.route('/:id(\\d+)')
   .get(ArtistController.getOne)
-  .patch(ArtistController.update)
+  .patch(upload.single('img'), ArtistController.update)
   .delete(ArtistController.delete);
 router.route('/')
   .get(ArtistController.getAll)
-  .post(ArtistController.create);
+  .post(upload.single('img'), ArtistController.create);
 
 export default router;
