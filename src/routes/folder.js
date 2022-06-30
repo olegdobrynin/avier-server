269cbf8dade96f306d4de68876b41498f0529a1f
Konstantin Kuzin <wearebeard@gmail.com>
2022-06-30 15:33:52 +0300

    feat: lint module and move to esm

--- src/routes/folder.js
+++ src/routes/folder.js
@@ -1,11 +1,12 @@
-const Router = require('express')
-const router = new Router()
-const folderController = require('../controllers/folderController')
+import { Router } from 'express';
+import folderController from '../controllers/folder.js';
 
-router.post('/', folderController.create)
-router.get('/', folderController.getAll)
-router.post('/', folderController.edit)
-router.delete('/', folderController.delete)
+const router = new Router();
 
+router.route('/')
+  .post(folderController.create)
+  .get(folderController.getAll)
+  .post(folderController.edit)
+  .delete(folderController.delete);
 
-module.exports = router
+export default router;
