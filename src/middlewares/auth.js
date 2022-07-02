1000eef452ec79ae4aa1c456635018e5a48c418d
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-02 17:08:31 +0300

    fix: when server error occurred send it to the next

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -14,7 +14,7 @@ export default (req, res, next) => {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
     next();
-  } catch (e) {
-    res.status(401).json({ message: 'Не авторизован' });
+  } catch (error) {
+    next(error);
   }
 };
