ecbe65953cfa7e6cf05a6d76abf4d638727e6718
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-10 21:04:45 +0300

    build: like controller

---
+++ src/controllers/like.js
@@ -0,0 +1,49 @@
+import models from '../models/index.js';
+
+const { Art, UserArtLike } = models;
+
+export default class LikeController {
+  static async like(req, res, next) {
+    try {
+      const { id, artId } = req.params;
+      await UserArtLike.create({ user_id: id, art_id: artId });
+
+      res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async unLike(req, res, next) {
+    try {
+      const { id, artId } = req.params;
+      await UserArtLike.destroy({ where: { user_id: id, art_id: artId } });
+
+      res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getAll(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { limit = 8, page = 1 } = req.query;
+      const offset = (page - 1) * limit;
+
+      const arts = await Art.findAndCountAll({
+        attributes: ['id', 'img', 'name'],
+        include: {
+          model: UserArtLike, as: 'likes', where: { user_id: id }, attributes: [],
+        },
+        order: [['id', 'DESC']],
+        limit,
+        offset,
+      });
+
+      res.json(arts);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
