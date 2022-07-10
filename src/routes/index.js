f86b6176ffade8ef05607fd54a82358c3cedde28
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-10 21:04:35 +0300

    feat: connect like router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import Router from 'express';
 import artRouter from './art.js';
 import artistRouter from './artist.js';
+import likeRouter from './like.js';
 import markRouter from './mark.js';
 import typeRouter from './type.js';
 import userRouter from './user.js';
@@ -9,6 +10,7 @@ const router = new Router();
 
 router.use('/art', artRouter);
 router.use('/artist', artistRouter);
+router.use('/like', likeRouter);
 router.use('/mark', markRouter);
 router.use('/type', typeRouter);
 router.use('/user', userRouter);
