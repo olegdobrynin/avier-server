3ab110bb0cc23e7092e78668fd678bde2e97eae0
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-28 01:29:15 +0300

    feat: add authorization to delete and post routes

--- src/routes/art.js
+++ src/routes/art.js
@@ -2,15 +2,16 @@ import { Router } from 'express';
 import multer from 'multer';
 import { defaultLimits, fileFilter } from '../helpers/multer.js';
 import ArtController from '../controllers/art.js';
+import authorization from '../middlewares/authorization.js';
 
 const router = new Router();
 const upload = multer({ fileFilter, limits: { ...defaultLimits, fields: 7, files: 5 } });
 
 router.route('/:id(\\d+)')
   .get(ArtController.getOne)
-  .delete(ArtController.delete);
+  .delete(authorization('admin', 'artist'), ArtController.delete);
 router.route('/')
   .get(ArtController.getAll)
-  .post(upload.array('img'), ArtController.create);
+  .post(authorization('admin', 'artist'), upload.array('img'), ArtController.create);
 
 export default router;
